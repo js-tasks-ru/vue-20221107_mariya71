29e58c701fde1decc46dd53bb00fee813d5804bf
19390998 <Beu?Px6w{c06>
2022-11-11 12:39:43 +0300

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,46 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+import {defineComponent} from "vue";
 
 // Создайте Vue приложение
+
+const App = defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: 'sum',
+    };
+  },
+
+  computed: {
+    resultValue() {
+      let i = 0;
+
+      switch (this.operator) {
+        case 'sum':
+          i = this.num1 + this.num2;
+          break;
+        case 'subtract':
+          i = this.num1 - this.num2;
+          break;
+        case 'multiply':
+          i = this.num1 * this.num2;
+          break;
+        case 'divide':
+          i = Math.round((this.num1 / this.num2) * 100) / 100;
+          break;
+        default:
+          i = this.num1 + this.num2;
+          break;
+      }
+
+      return i;
+    },
+  },
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
