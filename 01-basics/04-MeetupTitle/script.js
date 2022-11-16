725ab8fd7a93d96f048fd9fbc4f37f5ca06b68af
19390998 <Beu?Px6w{c06>
2022-11-16 15:05:30 +0300

    MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -15,3 +15,25 @@ function fetchMeetupById(meetupId) {
 }
 
 // Требуется создать Vue приложение
+
+const App = defineComponent({
+  'name': 'App',
+
+  data() {
+    return {
+      meetupId: 1,
+      meetupName: null,
+    };
+  },
+
+  watch: {
+    async meetupId(newValue, oldValue) {
+      let answer = await fetchMeetupById(newValue);
+      this.meetupName = answer.title;
+    },
+  },
+
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
