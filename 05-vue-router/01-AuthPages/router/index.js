8d1bca470798d5054dbc2fd9b1e606d3544f2fa9
19390998 <Beu?Px6w{c06>
2022-12-05 10:38:02 +0300

    AuthPages

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+export const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('../views/PageIndex'),
+  },
+  {
+    path: '/login:from?',
+    name: 'login',
+    component: () => import('../views/PageLogin'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/PageRegister'),
+  },
+];
+
 export const router = createRouter({
+  mode: 'history',
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  base: '/05-vue-router/01-AuthPages',
+  routes,
 });
